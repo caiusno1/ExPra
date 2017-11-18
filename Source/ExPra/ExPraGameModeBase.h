40ec7932139e3ce93b1a4e7e8198eabbdcbda7ce
Kai Biermeier <kaibm@mail.upb.de>
2017-11-18 16:53:30 +0100

    Init Prototype

---
+++ Source/ExPra/ExPraGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ExPraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPRA_API AExPraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
